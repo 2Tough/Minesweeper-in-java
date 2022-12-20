30d0dbc7f6390d98d496bb4ee1894ebe7cf96ee8
Polaski <vainferro@gmail.com>
2022-12-20 17:10:46 +0300

    Added new java classes

---
+++ app/src/main/java/com/example/minesweeper/MineGrid.java
@@ -0,0 +1,21 @@
+package com.example.minesweeper;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class MineGrid {
+    private List<Cell> cells;
+    private int size;
+
+    public MineGrid(int size) {
+        this.size = size;
+        cells = new ArrayList<>();
+        for (int i = 0; i < size *size ; i++) {
+            cells.add(new Cell(Cell.BLANK));
+        }
+    }
+
+    public List<Cell> getCells() {
+        return cells;
+    }
+}

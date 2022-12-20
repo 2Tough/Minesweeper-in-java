054c443c76bffcde95ff6c4c4141f742e19d21bb
2Tough <vainferro@gmail.com>
2022-12-21 00:24:49 +0300

    Added new variables and functions.

--- app/src/main/java/com/example/myminesweeper/MainActivity.java
+++ app/src/main/java/com/example/myminesweeper/MainActivity.java
@@ -1,20 +1,61 @@
 package com.example.myminesweeper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements OnCellClickListener {
+    RecyclerView gridRecyclerView;
+    MineGridRecyclerAdapter mineGridRecyclerAdapter;
+    MinesweeperGame game;
+    TextView smiley;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        smiley = findViewById(R.id.activity_main_grid);
+        smiley.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                game = new MinesweeperGame(10, 10);
+                mineGridRecyclerAdapter.setCells(game.getMineGrid().getCells());
+            }
+        });
+
+        gridRecyclerView = findViewById(R.id.activity_main_grid);
+        gridRecyclerView.setLayoutManager(new GridLayoutManager(this, 10));
+        game = new MinesweeperGame(10, 10);
+        mineGridRecyclerAdapter = new MineGridRecyclerAdapter(game.getMineGrid().getCells(), this);
+        gridRecyclerView.setAdapter(mineGridRecyclerAdapter);
     }
 
     @Override
     public void onCellClick(Cell cell) {
-        Toast.makeText(getApplicationContext(), "Cell clicked", Toast.LENGTH_SHORT).show();
+        game.handleCellClick(cell);
+
+        if(game.isGameOver()) {
+            Toast.makeText(getApplicationContext(),"Game is Over", Toast.LENGTH_SHORT).show();
+            game.getMineGrid().revealAllBombs();
+        }
+
+        mineGridRecyclerAdapter.setCells(game.getMineGrid().getCells());
+
+
+    }
+
+    @Override
+    public void cellClick(Cell cell) {
+
     }
 }
--- app/src/main/java/com/example/myminesweeper/MinesweeperGame.java
+++ app/src/main/java/com/example/myminesweeper/MinesweeperGame.java
@@ -1,12 +1,93 @@
 package com.example.myminesweeper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MinesweeperGame {
     private MineGrid mineGrid;
+    private boolean clearMode;
+    private boolean isGameOver;
+    
+    public MinesweeperGame(int size, int numberOfBombs)  {
+        this.clearMode = true;
+        this.isGameOver = false;
 
-    public MinesweeperGame(int size)  {
         mineGrid = new MineGrid(size);
+        mineGrid.generateGrid(numberOfBombs);
     }
+
+    public void handleCellClick(Cell cell){
+        if (!isGameOver) {
+            if(clearMode) {
+                clear(cell);
+            }
+        }
+
+    }
+
+    public void clear(Cell cell) {
+        int index = getMineGrid().getCells().indexOf(cell);
+        getMineGrid().getCells().get(index).setRevealed(true);
+
+        if (cell.getValue() == Cell.BOMB) {
+            boolean gameOver = true;
+        } else if (cell.getValue() == Cell.BLANK) {
+            List<Cell> toClear = new ArrayList<>();
+            List<Cell> toCheckAdjacents = new ArrayList<>();
+
+            toCheckAdjacents.add(cell);
+
+            while (toCheckAdjacents.size() > 0) {
+                Cell c = toCheckAdjacents.get(0);
+                int cellIndex = getMineGrid().getCells().indexOf(c);
+                int[] cellPos = getMineGrid().toXY(cellIndex);
+                for (Cell adjacent: getMineGrid().adjacentCells(cellPos[0], cellPos[1])) {
+                    if (adjacent.getValue() == Cell.BLANK) {
+                        if (!toClear.contains(adjacent)) {
+                            if (!toCheckAdjacents.contains(adjacent)) {
+                                toCheckAdjacents.add(adjacent);
+                            }
+                        }
+                    } else {
+                        if (!toClear.contains(adjacent)) {
+                            toClear.add(adjacent);
+                        }
+                    }
+                }
+                toCheckAdjacents.remove(c);
+                toClear.add(c);
+            }
+
+            for (Cell c: toClear) {
+                c.setRevealed(true);
+            }
+        } else if (cell.getValue() == Cell.BOMB) {
+            isGameOver = true;
+        }
+    }
+
+    public boolean isGameWon() {
+        int numbersUnrevealed = 0;
+        for (Cell c: getMineGrid().getCells()) {
+            if (c.getValue() != Cell.BOMB && c.getValue() != Cell.BLANK && !c.isRevealed()) {
+                numbersUnrevealed++;
+            }
+        }
+
+        if (numbersUnrevealed == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public boolean isGameOver() {
+        return isGameOver;
+    }
+
     public MineGrid getMineGrid() {
+
         return mineGrid;
     }
 }
